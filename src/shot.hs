bfb5329a09aee1e468c89d3cadbebbdbf1936e5c
William Gervasio <wilpgerv@telus.net>
2023-02-24 21:17:22 -0800

    added a hella number of tests

--- src/shot.hs
+++ src/shot.hs
@@ -6,6 +6,7 @@ module Shot where
     -- records outcome on the baord
     -- returns updated boat list and board
     checkShot :: (Integer, Integer) -> [Boat] -> Board -> ([Boat], Board)
+    checkShot _ [] board = ([], board)
     checkShot (x,y) boats board =
         let (updatedBoats, hit) = checkHitBoats (x,y) boats in
             if hit then
@@ -14,18 +15,35 @@ module Shot where
                 (boats, (updateBoardElement board (x,y) 'o')) ::([Boat], Board)
         
 
-        -- for the board
-        -- find tuple in board that matches (x,y)
-        -- if found, change char to 'x' if boat is hit
-        -- otherwise change char to 'o'
     
-    
-    
- 
+    checkHitBoat :: (Integer, Integer) -> Boat -> Boat
+    -- find tuple in boat that matches (x,y)
+    -- if found, change bool to True
+    -- return updated boat and bool if hit
+    checkHitBoat _ (Boat [] []) = (Boat [] []) 
+    checkHitBoat _ (Boat [] lst) = (Boat [] lst) 
+    checkHitBoat _ (Boat lst []) = (Boat lst []) 
+    checkHitBoat (xShot,yShot) (Boat ((xi,yi):xyRest) (hitI:hitRest)) = do
+        if (xShot == xi) && (yShot == yi) then
+            (Boat ((xi,yi):xyRest) (True:hitRest))
+        else 
+            let (Boat _ updatedHitRest) = checkHitBoat (xShot,yShot) (Boat xyRest hitRest)
+            in Boat ((xi,yi):xyRest) (hitI:updatedHitRest)
+
+
+    -- b1 = Boat [(1,1),(1,2),(1,3)] [False, False, False]
+    -- b2 = Boat [(10,10),(9,9),(8,8)] [False, True, False]
+    -- b3 = Boat [(5,4),(3,2),(1,0)] [True, True, True]
+    -- checkHitBoat (1,1) b1 -> true
+    -- checkHitBoat (9,9) b2 -> true
+    -- checkHitBoat (1,0) b3 -> true
+    -- checkHitBoat (1,1) b3 -> false
+    -- checkHitBoat (1,2) b2 -> false
+
     checkHitBoats :: (Integer, Integer) -> [Boat] -> ([Boat], Bool)
     checkHitBoats _ [] = ([], False)
-
     checkHitBoats (xShot,yShot) (headBoat:restBoats) = 
+        -- does not work if point was already hit
         let updatedBoat = checkHitBoat (xShot,yShot) headBoat in
         if headBoat /= updatedBoat then
             (updatedBoat:restBoats, True) :: ([Boat], Bool)
@@ -33,20 +51,18 @@ module Shot where
             let (updatedBoats, restHit::Bool) = checkHitBoats (xShot,yShot) restBoats in
                 (headBoat:updatedBoats, restHit) :: ([Boat], Bool)
 
-    
-    checkHitBoat :: (Integer, Integer) -> Boat -> Boat
-    checkHitBoat _ (Boat [] []) = (Boat [] []) 
-    -- find tuple in boat that matches (x,y)
-    -- if found, change bool to True
-    -- return updated boat and bool if hit
-    checkHitBoat _ (Boat [] lst) = (Boat [] lst) 
-    checkHitBoat _ (Boat lst []) = (Boat lst []) 
-    checkHitBoat (xShot,yShot) (Boat ((xi,yi):xyRest) (hitI:hitRest)) = do
-        if (xShot == xi) && (yShot == yi) then
-            (Boat ((xi,yi):xyRest) (True:hitRest))
-        else 
-            let (Boat xyRest hitRest) = checkHitBoat (xShot,yShot) (Boat xyRest hitRest)
-            in Boat ((xi,yi):xyRest) (hitI:hitRest)
+    -- lob1 = []
+    -- lob2 = [b1]
+    -- lob3 = [b1, b2]
+    -- lob4 = [b1, b2, b3]
+
+
+    -- checkHitBoats (1,1) lob1 -> ([], False)
+    -- checkHitBoats (1,1) lob2 -> ([Boat [(1,1),(1,2),(1,3)] [True,False,False]], True)
+    -- checkHitBoats (8,8) lob3 -> ([Boat [(1,1),(1,2),(1,3)] [False,False,False],Boat [(10,10),(9,9),(8,8)] [False,True,True]], True)
+    -- could be an error if not handled:
+    -- checkHitBoats (1,0) lob4 -> ([Boat [(1,1),(1,2),(1,3)] [False,False,False],Boat [(10,10),(9,9),(8,8)] [False,True,False],Boat [(5,4),(3,2),(1,0)] [True,True,True]], False)
+    -- checkHitBoats (1,5) lob4 -> ([Boat [(1,1),(1,2),(1,3)] [False,False,False],Boat [(10,10),(9,9),(8,8)] [False,True,False],Boat [(5,4),(3,2),(1,0)] [True,True,True]], False)
 
     -- prompts user for row and col position of shot
     -- checks if shot is valid
